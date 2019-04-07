b6444178bf4c1c5137496a927b46fa28073da070
Mritunjai Pratap Singh <Mritunjai_PratapSing@Dell.com>
2019-04-07 11:45:58 +0530

    Basic Project setup

---
+++ ExceptionHandling/ProjectBasicSetup/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace ProjectBasicSetup.Extensions
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// we need to configure CORS in our application. CORS (Cross-Origin Resource Sharing) is a mechanism that gives rights to the user to access resources from the 
+        /// server on a different domain. Because we will use Angular as a client-side on a different domain than server’s domain, configuring CORS is mandatory.
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            //We are using the basic settings for adding CORS policy because for this project allowing any origin, method, and header is quite enough. 
+            //But you can be more restrictive with those settings if you want. Instead of the AllowAnyOrigin() method which allows requests from any source, 
+            //you could use the WithOrigins("http://www.something.com") which will allow requests just from the specified source. 
+            //Also, instead of AllowAnyMethod() that allows all HTTP methods,  you can use WithMethods("POST", "GET") that will allow only specified HTTP methods.
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+        /// <summary>
+        /// Inject all services here instead of doing it in Startup.cs
+        /// </summary>
+        /// <param name="services"></param>
+        public static void InjectServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ ExceptionHandling/ProjectBasicSetup/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ProjectBasicSetup.Extensions;
+
+namespace ProjectBasicSetup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureCors();
+
+            services.InjectServices();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
+            //will forward proxy headers to the current request. This will help us during the Linux deployment.
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            //enables using static files for the request. If we don’t set a path to the static files, 
+            //it will use a wwwroot folder in our solution explorer by default.
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
